2ee40cbe4c3d0684861e78ae1ae87a30210273e4
Diego Vega <diego.vega.camacho@gmail.com>
2023-11-07 09:25:53 -0600

    initialize project template

---
+++ tc2008B_server.py
@@ -0,0 +1,52 @@
+# TC2008B Modelación de Sistemas Multiagentes con gráficas computacionales
+# Python server to interact with Unity via POST
+# Sergio Ruiz-Loza, Ph.D. March 2021
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import json
+
+class Server(BaseHTTPRequestHandler):
+    
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+    def do_GET(self):
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        position = {
+            "x" : 1,
+            "y" : 2,
+            "z" : 3
+        }
+
+        self._set_response()
+        self.wfile.write(str(position).encode('utf-8'))
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8585):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info("Starting httpd...\n") # HTTPD is HTTP Daemon!
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:   # CTRL+C stops the server
+        pass
+    httpd.server_close()
+    logging.info("Stopping httpd...\n")
+
+if __name__ == '__main__':
+    from sys import argv
+    
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
+
+
+
